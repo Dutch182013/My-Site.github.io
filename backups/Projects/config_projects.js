d74e8c87c0638d37aeb69f24f166510ebbc6915e
Dutch <96445963+Dutch182013@users.noreply.github.com>
2025-09-10 14:44:59 +0700

    Add files via upload

--- backups/Projects/config_projects.js
+++ backups/Projects/config_projects.js
@@ -3,23 +3,23 @@ class Project_Config {
         this.project_config_ID = document.getElementById("project_configs");
 
     }
+    text_href(project_url) {
+        return `${project_url}/index.html`;
+    }
+    text_title(project_name) {
+        return project_name;
+    }
     pack_data(href, title) {
         return `<center><span class="typing-2"></span><a href="${href}">${title}</a></center>`
     }
-    load_list() {
+    load_list(list_json="list.json", path_prefix="") {
         let text_list = ""
-        let text_href = (project_url) => {
-            return `${project_url}/index.html`;
-        }
-        let text_title = (project_name) => {
-            return project_name;
-        }
-        fetch("list.json")
+        fetch(`${path_prefix}${list_json}`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 for (let i = 0; i < data.length; i++) {
-                    text_list += this.pack_data(text_href(data[i].url), text_title(data[i].name));
+                    text_list += this.pack_data(this.text_href(`${path_prefix}${data[i].url}`), this.text_title(data[i].name));
                 }
                 this.project_config_ID.innerHTML = text_list;
             })
