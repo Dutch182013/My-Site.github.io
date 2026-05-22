ee9564e2393a269703d8aa093c12a177c39bce43
Dutch <96445963+Dutch182013@users.noreply.github.com>
2026-05-22 10:14:22 +0700

    Add files via upload

--- backups/Projects/My-Posts/main.js
+++ backups/Projects/My-Posts/main.js
@@ -1,18 +1,50 @@
 
-const drawPosts = document.getElementById('draw-posts');
-
-fetch('posts.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(post => {
-      const postElement = document.createElement('div');
-      postElement.classList.add('post');
-      postElement.innerHTML = `
-        <h2>${post.title}</h2>
-        <p>${post.content}</p>
-        <span>${post.date}</span>
-      `;
-      drawPosts.appendChild(postElement);
+async function api_drive(url = "https://script.google.com/macros/s/AKfycbxJZbr9ZasslW2vEUw6-WBzWoMse-r25vEYaRF-dT4PjWDmxC55HsSL6kmPiLs6MoBQ/exec") {
+  let output
+  await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      output = data;
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
     });
-  })
-  .catch(error => console.error('Error fetching posts:', error));
+  return output
+}
+
+
+function onclick_set(url) {
+  window.open(url)
+}
+
+class Post {
+  constructor() {
+    this.posts = []
+    this.draw = document.getElementById("draw-posts")
+  }
+  async createPost(dive = api_drive()) {
+    let output = "";
+    let group = [];
+    for (let data = 0; data < (await dive).length; data++) {
+      group.push({
+        "name": (await dive)[data].name,
+        "link": (await dive)[data].directUrl
+      });
+      this.posts.push({
+        "name": (await dive)[data].name,
+        "link": (await dive)[data].directUrl
+      });
+      output += `<div class="max-width"> ${(await dive)[data].name} <br> <img src="${(await dive)[data].directUrl}" width="100" height="100" style="width:100%;height:100%;" onclick="onclick_set();"></div>`
+    }
+    this.draw.innerHTML = output
+  }
+}
+
+async function main() {
+  let post = new Post();
+  await post.createPost();
+
+  console.log(post.posts);
+}
+
+main();
