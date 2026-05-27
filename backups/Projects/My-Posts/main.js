3571cf93d0c610d40e0d6622b9a83aeaf91b0ffb
Dutch <96445963+Dutch182013@users.noreply.github.com>
2026-05-27 19:15:49 +0700

    Add files via upload

--- backups/Projects/My-Posts/main.js
+++ backups/Projects/My-Posts/main.js
@@ -1,31 +1,42 @@
+const POST_API =
+  "https://script.google.com/macros/s/AKfycbxJZbr9ZasslW2vEUw6-WBzWoMse-r25vEYaRF-dT4PjWDmxC55HsSL6kmPiLs6MoBQ/exec";
+const POST_ID = document.getElementById("drawPosts");
+
+async function setResponse(setAPI_URL) {
+  return await fetch(setAPI_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let html = "";
+      data.forEach((post) => {
+        html += `<div class="left"><iframe class="img" src="${post.directUrl}" frameborder="0"></iframe></div>`;
+        html += `<div class="right">`;
+        html += `<p class="text">${post.name}</p>`;
+        html += `<a href="${post.directUrl}" target="_blank">View Image</a>`;
+        html += `</div>`;
+      });
+      return html;
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+}
+
 async function main() {
   try {
-    const POST_API =
-      "https://script.google.com/macros/s/AKfycbxJZbr9ZasslW2vEUw6-WBzWoMse-r25vEYaRF-dT4PjWDmxC55HsSL6kmPiLs6MoBQ/exec";
-
-    const response = await fetch(POST_API, { method: "GET" });
-    const posts = await response.json();
-
-    const post_id = document.getElementById("drawPosts");
-    let item = (name, size, url) => {
-      return `<div class="rigth"><a href="${url}">${name}</a></div>`;
-    };
-
-    let draw_output;
-    for (let i = 0; i < posts.length; i++) {
-      let post = await posts[i];
-      draw_output += `${(await item)(await post.name, await post.size, await post.directUrl)}`;
-      console.log(post);
-    }
-    post_id.innerHTML = await draw_output;
+    POST_ID.innerHTML = await setResponse(POST_API);
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 }
 
 main();
 
 // {
+// https://drive.google.com/uc?export=view&id=
 //     "name": "IMG_20260501_061404_668.jpg",
 //     "id": "15KRHH34a2rutxfQVp6Zzm4QbvdMG3FwL",
 //     "size": 1571200,
